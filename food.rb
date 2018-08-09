8f361da91dfdd6e2dc72624ec374de51b8174f9e
elanapocress <pocresse@gmail.com>
2018-08-09 15:00:04 +0000

    automatically backed up by learn

---
+++ food.rb
@@ -0,0 +1,9 @@
+def sandwich_request(ingredients, order) 
+  ingredients = [
+    ["rye", "sourdough", "baguette"],
+    ["ham", "salami", "turkey"],
+    ["swiss", "munster", "cheddar"],
+    ["mayo", "mustard", "tabasco"]
+].include? rye
+ end 
+ sandwhich_request
