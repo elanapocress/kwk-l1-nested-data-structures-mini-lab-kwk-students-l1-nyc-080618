e2da13c8cbc47f0cd9c95c02276dad67ea6821ed
elanapocress <pocresse@gmail.com>
2018-08-09 15:10:22 +0000

    automatically backed up by learn

--- index.rb
+++ index.rb
@@ -7,12 +7,8 @@ ingredients = [
 ]
 []
 def sandwich_request(ingredients, order) 
-  ingredients = [
-    ["rye", "sourdough", "baguette"],
-    ["ham", "salami", "turkey"],
-    ["swiss", "munster", "cheddar"],
-    ["mayo", "mustard", "tabasco"]
-]
+  ingredients 
+  if 
  end 
 # example city_info hash
 cities = {
