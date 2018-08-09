ccdc967b849b8adf429b8c5523765783a7edf78f
elanapocress <pocresse@gmail.com>
2018-08-09 15:01:07 +0000

    automatically backed up by learn

--- food.rb
+++ food.rb
@@ -1,7 +1,9 @@
-
+def food
   ingredients = [
     ["rye", "sourdough", "baguette"],
     ["ham", "salami", "turkey"],
     ["swiss", "munster", "cheddar"],
     ["mayo", "mustard", "tabasco"]
 ].include? rye
+end 
+food
