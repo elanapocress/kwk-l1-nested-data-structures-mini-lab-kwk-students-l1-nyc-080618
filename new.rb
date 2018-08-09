a948dc04a1ea802924c8184067efa83fd12bf948
elanapocress <pocresse@gmail.com>
2018-08-09 15:38:16 +0000

    automatically backed up by learn

---
+++ new.rb
@@ -0,0 +1,25 @@
+def city_populations(cities)
+  cities = {
+  :new_york => { 
+    :mayor => "Bill DeBlasio",
+    :population => 8337000,
+    :website => "http://www.nyc.gov"
+  },
+  :los_angeles => { 
+    :mayor => "Eric Garcetti",
+    :population => 3884307,
+    :website => "http://www.lacity.org"
+  },
+  :miami => { 
+    :mayor => "Tomás Regalado",
+    :population => 419777,
+    :website => "http://www.miamigov.com"
+  },
+  :chicago => { 
+    :mayor => "Rahm Emanuel",
+    :population => 2695598,
+    :website => "http://www.cityofchicago.org/"
+  }
+}
+cities[:new_york][:population] + cities[:los_angeles][:population] +cities[:miami][:population] +cities[:chicago][:population]
+end
